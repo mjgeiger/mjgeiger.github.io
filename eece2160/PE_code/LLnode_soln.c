9f2a0506a4776057e171b468129fd3d6e0a41a24
mjgeiger <Michael_Geiger@uml.edu>
2017-12-11 23:31:17 -0500

    12/11 web update

--- eece2160/PE_code/LLnode_soln.c
+++ eece2160/PE_code/LLnode_soln.c
@@ -7,14 +7,13 @@
 
    Contains function definitions for functions
    prototyped in LLnode.h
-
-   INSTRUCTOR SOLUTION
 **************************************************/
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "LLnode.h"
 
+// Add integer to list and return list's starting address
 LLnode *addNode(LLnode *list, int v) {
 	LLnode *newNode;
 
@@ -34,55 +33,67 @@ LLnode *addNode(LLnode *list, int v) {
 // Find node in list and return pointer
 //   (or NULL if not found)
 LLnode *findNode(LLnode *list, int v) {
-	LLnode *n;
-
-	n = list;		// Start with first node
-
-	while (n != NULL) {			// Search until after last node
-		if (n->value == v)		// Data found--return n
+	// Create pointer to start of list
+	LLnode *n = list;
+	
+	// Loop to visit all nodes in list
+	while (n != NULL) {
+		// For current node:
+		//   a. Check if data you want is in that node --> return address if so
+		if (v == n->value) {
 			return n;
+		}
+		
+		//   b. Otherwise, move to the next node
 		n = n->next;
 	}
-	return NULL;				// If you get here, data wasn't found
+	
+	// If data isn't found, return NULL
+	return NULL;
 }
 
 // Delete node from list (if present)
+// Return starting address of list when done
 LLnode *delNode(LLnode *list, int v) {
-	LLnode *cur = list;		// Pointer to current node--initially start of list
-	LLnode *prev = NULL;	// Pointer to node before cur--initially NULL
-
-	// Loop will search list, stopping either 
-	//   when list ends or value is found
-	while ((cur != NULL) && (cur->value != v)) {
+	LLnode *prev = NULL;
+	LLnode *cur = list;
+	
+	while (cur != NULL && v != cur->value) {
 		prev = cur;
 		cur = cur->next;
 	}
 	
-	// Data wasn't found--return unmodified list
-	if (cur == NULL)
+	if (cur == NULL)		// No match--went through entire list
 		return list;
-
-	// Data is in first node--must change pointer to start of list
-	if (prev == NULL)
-		list = list->next;
-
-	// Data is in some other node
-	// Set next pointer in prev (node before one being
-	//   removed) to point past node being removed
-	else
-		prev->next = cur->next;
-
-	free(cur);
-	return list;
+	
+	else {			// Match found (v == cur->value), so delete cur node
+		
+		if (cur == list)		// Match found in first node
+								// Alternate condition: prev == NULL
+			list = cur->next;
+		
+		else		// Match found outside of first node
+			prev->next = cur->next;
+		
+		free(cur);
+		return list;
+	}
 }
 
 // Print contents of entire list
 void printList(LLnode *list) {
+	// Create pointer to start of list
 	LLnode *n = list;
+	
+	if (n == NULL)
+		printf("List is empty\n");
 
-	while (n != NULL) {
-		printf("%d\n", n->value);
-		n = n->next;
+	else {
+		// Loop to visit all nodes in list
+		while (n != NULL) {
+			printf("%d\n", n->value);
+			n = n->next;
+		}
 	}
 }
 
@@ -90,56 +101,57 @@ void printList(LLnode *list) {
 //   and add integer; return pointer to
 //   start of list
 LLnode *addSortedNode(LLnode *list, int v) {
-	LLnode *newNode;
-	LLnode *cur = list; 
 	LLnode *prev = NULL;
-
+	LLnode *cur = list;
+	LLnode *newNode;
+	
 	// Allocate space for new node; exit if error
 	newNode = (LLnode *)malloc(sizeof(LLnode));
 	if (newNode == NULL) {
 		fprintf(stderr, "Error: could not allocate new node\n");
+		freeList(list);
 		exit(0);
 	}
-
+	
 	newNode->value = v;		// Copy value to new node
-
-	// Search list to find right spot
-	// When done, prev will point to spot before new 
-	//   node, and cur will point to spot after it
-	while ((cur != NULL) && (cur->value < v)) {
+	
+	// Search for first node to follow new node
+	while (cur != NULL && cur->value <= v) {
 		prev = cur;
 		cur = cur->next;
 	}
 	
-	// Special case: new node goes at start of list
-	if (prev == NULL) {
-		newNode->next = cur;
+	// Make new node point to whatever follows it
+	newNode->next = cur;
+	
+	// Make node before new one point to it
+	if (prev == NULL)		// New node is first
 		list = newNode;
-	}
-
-	// Otherwise, node goes somewhere in middle
-	else {
+	else					// New node is after first node
 		prev->next = newNode;
-		newNode->next = cur;
-	}
+	
 	return list;
 }
 
 // Find node in sorted list and return pointer
 //   (or NULL if not found)
 LLnode *findSortedNode(LLnode *list, int v) {
-	LLnode *n;
-
-	n = list;		// Start with first node
-
-	// Search until after last node, or point at 
-	//  which v can't possibly be in list
-	while ((n != NULL) && (n->value <= v)) {	
-		if (n->value == v)		// Data found--return n
+	// Create pointer to start of list
+	LLnode *n = list;
+	
+	// Loop to visit all nodes in list
+	while (n != NULL && n->value <= v) {
+		// Check if data you want is in current node --> return address if so
+		if (v == n->value) {
 			return n;
+		}
+		
+		// Otherwise, move to the next node
 		n = n->next;
 	}
-	return NULL;				// If you get here, data wasn't found
+	
+	// If data isn't found, return NULL
+	return NULL;
 }
 
 // Frees entire list before end of program to avoid memory leaks
